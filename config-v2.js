46b01885c0d5bbe8197fc3bbc5a1e31dcb48fb3b
Mahiya1234567 <ransarapavaridu70@gmail.com>
2024-12-21 21:21:20 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "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"
+const BOT_NUMBER = "94724501649"
+const SESSION_ID = "PRABATH-MD~g2lBESjR#xFct5xHmpZL82H1sATwgWswkY4n9G-ynmIK20VOqGk8"
 
 
 module.exports = {
